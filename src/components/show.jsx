b717778d05a02d8c0a20d9a332f5256909f330fa
peterhwang2010 <inhohwang2010@gmail.com>
2015-09-12 22:25:42 -0400

    added boilerplates for all components

---
+++ src/components/show.jsx
@@ -0,0 +1,11 @@
+var React = require('react');
+var header = require('./header');
+
+module.exports = React.createClass({
+	render: function(){
+		return <div>
+			<Header />
+			I am show page 
+		</div>
+	}
+});
